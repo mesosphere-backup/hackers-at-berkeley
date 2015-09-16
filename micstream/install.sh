75a47c347fd9e1e7a2bab5cc0e56071101ab45b8
Sunil Shah <sunil@mesosphere.io>
2015-09-16 14:14:19 -0700

    Fix typo.

--- micstream/install.sh
+++ micstream/install.sh
@@ -9,7 +9,7 @@ fi
 git clone /home/pi/hackers-at-berkeley git@github.com:mesosphere/hackers-at-berkeley.git
 
 # Install into crontab
-echo "@reboot /home/pi/hackers-at-berkeley/micstream/splmeter.py -H \"hackers-at-berkeley.mesosphere.io\" -i 1 -c hw:1 -p 8088 >> /home/pi/splmeter.out" >> /var/sool/cron/crontabs/root
+echo "@reboot /home/pi/hackers-at-berkeley/micstream/splmeter.py -H \"hackers-at-berkeley.mesosphere.io\" -i 1 -c hw:1 -p 8088 >> /home/pi/splmeter.out" >> /var/spool/cron/crontabs/root
 
 # Restart
 reboot
