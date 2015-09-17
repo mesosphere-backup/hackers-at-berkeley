f24f3d766ba48d654e39320ccc9e2dd177433ce8
Tyler Neely <t@jujit.su>
2015-09-16 19:52:10 -0700

    improve front-end example

--- examples/frontend_service/front_end.py
+++ examples/frontend_service/front_end.py
@@ -1,16 +1,32 @@
+import random
+
 from flask import Flask, render_template, jsonify
 import requests
 import srvlookup
 
+app = Flask(__name__)
+
 def lookup_backends():
     return srvlookup.lookup('example_dcos_backend', 'tcp', 'marathon.mesos')
 
-app = Flask(__name__)
-
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/read')
+def read():
+    return jsonify({"results":[
+        {
+            "host": "host:02",
+            "values": [random.randint(0,100) for i in xrange(10)]
+        }
+    ]})
     # scatter-gather requests to each back-end
     results = []
     for backend in lookup_backends():
-        results.append(requests.get("%s:%s" % (backend.host, backend.port)).json())
+        endpoint = "%s:%s" % (backend.host, backend.port)
+        results.append({
+            "host": endpoint,
+            "values": requests.get(endpoint).json(),
+        })
     return jsonify(results)
