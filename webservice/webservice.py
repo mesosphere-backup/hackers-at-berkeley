fbf28a631a99bb9d006de2847d58dfa5e1771266
Greg Mann <gregorywmann@gmail.com>
2015-09-16 20:19:12 -0700

    Changed port in webservice's marathon.json.

--- webservice/webservice.py
+++ webservice/webservice.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     # In a real environment, never run with debug=True
     # because it gives you an interactive shell when you
     # trigger an unhandled exception.
-    app.run(host="0.0.0.0", debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=True, port=80)
 
