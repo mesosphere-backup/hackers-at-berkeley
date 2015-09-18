ec481ff3ba32a8e2a1522bb1e41b1ea7b3dcadda
Greg Mann <gregorywmann@gmail.com>
2015-09-17 17:17:36 -0700

    Changed port for frontend_service.

--- perimeter/perimeter.py
+++ perimeter/perimeter.py
@@ -39,7 +39,7 @@ producer = SimpleProducer(kafka, async=True)
 
 app = Flask(__name__)
 
-default_ranges = {"intensity": [300, 3000],
+default_ranges = {"intensity": [0, 2500],
                   "x": [0, 100],
                   "y": [0, 25]}
 
