a67eafdc17bf0ca29aab6a6521d6b78ef4076e7f
Greg Mann <gregorywmann@gmail.com>
2015-09-24 08:29:06 -0700

    Commented out kafka code.

--- perimeter/perimeter.py
+++ perimeter/perimeter.py
@@ -2,7 +2,7 @@ import sys
 
 from cassandra.cluster import Cluster
 from flask import Flask, render_template, jsonify
-from kafka import SimpleProducer, KafkaClient
+# from kafka import SimpleProducer, KafkaClient
 import srvlookup
 
 # This uses mesos-dns to locate our cassandra cluster.  Cassandra
@@ -28,14 +28,14 @@ def try_setup():
 
 # Kafka may not always run on the same port, so we need to perform
 # an SRV record lookup in order to find it.
-kafka_location = srvlookup.lookup('broker-0', 'tcp', 'kafka.mesos')[0]
-kafka = KafkaClient("%s:%s" % (kafka_location.host, kafka_location.port))
+# kafka_location = srvlookup.lookup('broker-0', 'tcp', 'kafka.mesos')[0]
+# kafka = KafkaClient("%s:%s" % (kafka_location.host, kafka_location.port))
 
 # Real-world Kafka workloads will gain an order of magnitude++
 # more throughput when using async mode.  The trade-off is your
 # requests may have higher latency (the cli will instantly return
 # however.)  This is the classic throughput-latency trade-off at play.
-producer = SimpleProducer(kafka, async=True)
+# producer = SimpleProducer(kafka, async=True)
 
 app = Flask(__name__)
 
